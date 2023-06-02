e10b5f845d649b7c69b2d2c0df402111742b9736
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-02 10:36:38 -0300

    chore: hardcode api into variable

--- src/core/services/apolloService.ts
+++ src/core/services/apolloService.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
+  uri: 'http://172.245.142.218:5000/graphql',
   cache: new InMemoryCache({
     resultCaching: true,
   }),
