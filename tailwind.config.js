4c0d265128d94b13ff8c2bb33ff5abb60f14e359
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-03 08:33:38 -0300

    style: more columns

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
         win: '#00FF19',
         loss: '#FF0000',
         subtle: '#7D8A88'
+      },
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+        '14': 'repeat(14, minmax(0, 1fr))',
+        '15': 'repeat(15, minmax(0, 1fr))',
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '17': 'repeat(17, minmax(0, 1fr))',
+        '18': 'repeat(18, minmax(0, 1fr))',
+        '19': 'repeat(19, minmax(0, 1fr))',
+        '20': 'repeat(20, minmax(0, 1fr))',
       }
     },
   },
