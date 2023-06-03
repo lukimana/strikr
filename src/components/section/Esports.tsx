65de60a0c70fb6b01ac667e480d64471ba6349a3
Nodgear <matheus.silva3@ftc.edu.br>
2023-06-03 09:06:12 -0300

    fix: empty pages

--- src/components/section/Esports.tsx
+++ src/components/section/Esports.tsx
@@ -3,7 +3,7 @@ interface IEsportsSectionProps {
 
 const EsportsSection: React.FunctionComponent<IEsportsSectionProps> = (props) => {
   return <section
-    className='flex flex-col gap-8 px-20 pb-28'
+    className='flex flex-col gap-8 px-8 sm:px-20'
   >
     <h3 
       className='text-lg font-semibold'
---
+++ src/pages/characters.tsx
@@ -0,0 +1,13 @@
+import GeneralLayout from '@/components/layout/General'
+import Head from 'next/head'
+
+export default function Strikers() {
+  return <>
+    <Head>
+      <title>Strikr</title>
+      <meta name="description" content="OS Statistics & community hub." />
+    </Head>
+    <GeneralLayout>
+    </GeneralLayout>
+  </>
+}
