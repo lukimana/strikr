5c3dd37d8c73b0dcb17121f5350ec0fc671591f9
Nodgear <wbm.nodge@gmail.com>
2023-06-24 20:47:29 -0300

    fix: title keys

--- src/core/relations/objects/titles.ts
+++ src/core/relations/objects/titles.ts
@@ -1,190 +1,64 @@
 const lookup = {
-  'TitleData_KOChamp': {
-    en: 'K.O Champion'
-  },
-  'ScalingForLateGameTitle': {
-    en: 'Scaling For Late Game',
-  },
-  'ProductionTitle': {
-    en: 'The Eternal Flame',
-  },
-  'FounderTitle': {
-    en: 'Founding Member',
-  },
-  'RookieRoadTitle': {
-    en: 'Competitor',
-  },
-  'BPS1Title1': {
-    en: 'Beta Season Striker',
-  },
-  'BPS1Title2': {
-    en: 'The Early Investor',
-  },
-  'BPS2Title1': {
-    en: 'Season Two Striker',
-  },
-  'BPS2Title2': {
-    en: 'The Hotshot',
-  },
-  'RankedChampionTitle': {
-    en: 'Season 1 Omega Striker',
-  },
-  'TournamentWinnerTitle': {
-    en: 'S1 Tournament Winner',
-  },
-  'TournamentFlawlessTitle': {
-    en: 'S1 Flawless Tournament Winner',
-  },
-  'TournamentTitle1': {
-    en: 'Season 1 Tournament Warrior',
-  },
-  'TournamentTitle2': {
-    en: 'Season 1 Tournament Legend',
-  },
-  'TournamentTitle3': {
-    en: 'Season 1 Tournament Champion',
-  },
-  'TournamentShadowTitle1': {
-    en: 'Shadow Helper',
-  },
-  'TournamentShadowTitle2': {
-    en: 'Shadow Warrior',
-  },
-  'TournamentShadowTitle3': {
-    en: 'Shadow Hero',
-  },
-  'TournamentShadowTitle4': {
-    en: 'The Selfless',
-  },
-  'TournamentShadowTitle5': {
-    en: 'World\'s Best Loser',
-  },
-  'SniperBrawlerEventTitle1': {
-    en: 'Expert Brawler',
-  },
-  'SniperBrawlerEventTitle2': {
-    en: 'Expert Sniper',
-  },
-  'JulietteMasteryTitle': {
-    en: 'Rising Star',
-  },
-  'KaiMasteryTitle': {
-    en: 'Prodigy',
-  },
-  'DubuMasteryTitle': {
-    en: 'The Most Huggable',
-  },
-  'EstelleMasteryTitle': {
-    en: 'Fashion Icon',
-  },
-  'DrekarMasteryTitle': {
-    en: 'Space Invader',
-  },
-  'AtlasMasteryTitle': {
-    en: 'Time Traveler',
-  },
-  'JunoMasteryTitle': {
-    en: 'Blobbo Buddy',
-  },
-  'RuneMasteryTitle': {
-    en: 'Sinister Shadow',
-  },
-  'EraMasteryTitle': {
-    en: 'Bewitched',
-  },
-  'AiMiMasteryTitle': {
-    en: 'Glitchy',
-  },
-  'AsherMasteryTitle': {
-    en: 'Unbreakable',
-  },
-  'ZentaroMasteryTitle': {
-    en: 'Blade Master',
-  },
-  'RasmusMasteryTitle': {
-    en: 'Off the Hook',
-  },
-  'LunaMasteryTitle': {
-    en: 'Chaotic',
-  },
-  'XMasteryTitle': {
-    en: 'Excessive',
-  },
-  'ZipMasteryTitle': {
-    en: 'Virtuoso',
-  },
-  'GravityMageMasteryTitle': {
-    en: 'Illusionist',
-  },
-  'S1ChampionTitle': {
-    en: 'Beta Season Omega Striker',
-  },
-  'S1TournamentFlawlessTitle': {
-    en: 'S1 Flawless Tournament Winner',
-  },
-  'S1TournamentChampionTitle': {
-    en: 'S1 Tournament Champion',
-  },
-  'S1TournamentLegendTitle': {
-    en: 'S1 Tournament Legend',
-  },
-  'S1TournamentWarriorTitle': {
-    en: 'S1 Tournament Warrior',
-  },
-  'HalloweenTitle1': {
-    en: 'Pumpkin',
-  },
-  'HalloweenTitle2': {
-    en: 'Spooky',
-  },
-  'LaunchBattlePassTitle1': {
-    en: 'Rise & Grind',
-  },
-  'LaunchBattlePassTitle2': {
-    en: 'Built Different',
-  },
-  'LaunchBattlePassTitle3': {
-    en: 'Sigma Striker',
-  },
-  'LaunchBattlePassTitle4': {
-    en: 'Cracked',
-  },
-  'LaunchBattlePassTitle5': {
-    en: 'Cringe',
-  },
-  'LaunchBattlePassTitle6': {
-    en: 'K.O. Champ',
-  },
-  'LaunchBattlePassTitle7': {
-    en: 'Trailblazer',
-  },
-  'HyperXTitle': {
-    en: 'HyperX Hero',
-  },
-  'AbandonedTitle': {
-    en: 'Abandoned',
-  },
-  'IdolTitle': {
-    en: 'Idol',
-  },
-  'PerformerTitle': {
-    en: 'Performer',
-  },
-  'TeaTimeJellyTitle': {
-    en: 'Jelly',
-  },
-  'TeaTimeCaffeinatedTitle': {
-    en: 'Caffeinated',
-  },
-  'TeaTimeBobaTitle': {
-    en: 'Boba',
-  },
-  'TeamRockOniTItle': {
-    en: 'Vice Versa',
-  },
-  'TeamEDMOniTitle': {
-    en: 'Thousand Absolutes',
-  },
+  TitleData_Abandoned: { en: 'Abandoned' },
+  TitleData_Aesop: { en: 'Aesop' },
+  TitleData_AngelicSupportMastery: { en: 'Time Traveler' },
+  TitleData_BetaReward: { en: 'The Eternal Flame' },
+  TitleData_BPS1Investor: { en: 'The Early Investor' },
+  TitleData_BPS1Striker: { en: 'Beta Season Striker' },
+  TitleData_BPS2Hotshot: { en: 'The Hotshot' },
+  TitleData_BPS2Striker: { en: 'Season Two Striker' },
+  TitleData_BuiltDifferent: { en: 'Built Different' },
+  TitleData_ChaoticRocketeerMastery: { en: 'Chaotic' },
+  TitleData_CleverSummonerMastery: { en: 'Blobbo Buddy' },
+  TitleData_Competitor: { en: 'Competitor' },
+  TitleData_Cracked: { en: 'Cracked' },
+  TitleData_Cringe: { en: 'Cringe' },
+  TitleData_EDMOniMastery: { en: 'Virtuoso' },
+  TitleData_EmpoweringEnchanterMastery: { en: 'Bewitched' },
+  TitleData_FlashySwordsmanMastery: { en: 'Blade Master' },
+  TitleData_FlexibleBrawlerMastery: { en: 'Rising Star' },
+  TitleData_Founder: { en: 'Founding Member' },
+  TitleData_GravityMageMastery: { en: 'Illusionist' },
+  TitleData_Halloween_Pumpkin: { en: 'Pumpkin' },
+  TitleData_Halloween_Spooky: { en: 'Spooky' },
+  TitleData_HulkingBeastMastery: { en: 'Excessive' },
+  TitleData_HyperX: { en: 'HyperX Hero' },
+  TitleData_Idol: { en: 'Idol' },
+  TitleData_KOChamp: { en: 'K.O. Champ' },
+  TitleData_MagicalPlaymakerMastery: { en: 'Glitchy' },
+  TitleData_ManipulatingMastermindMastery: { en: 'Sinister Shadow' },
+  TitleData_MasterBrawler: { en: 'Expert Brawler' },
+  TitleData_MasterSniper: { en: 'Expert Sniper' },
+  TitleData_NimbleBlasterMastery: { en: 'Space Invader' },
+  TitleData_None: { en: 'No title :(' },
+  TitleData_Performer: { en: 'Performer' },
+  TitleData_RiseandGrind: { en: 'Rise & Grind' },
+  TitleData_RockOniMastery: { en: 'Rock Star' },
+  TitleData_S1Champion: { en: 'Beta Season Omega Striker' },
+  TitleData_S1TournamentChampion: { en: 'S1 Tournament Champion' },
+  TitleData_S1TournamentFlawless: { en: 'S1 Flawless Tournament Winner' },
+  TitleData_S1TournamentLegend: { en: 'S1 Tournament Legend' },
+  TitleData_S1TournamentWarrior: { en: 'S1 Tournament Warrior' },
+  TitleData_S1TournamentWinner: { en: 'S1 Tournament Winner' },
+  TitleData_ScalingForLateGame: { en: 'Scaling For Late Game' },
+  TitleData_ShieldUserMastery: { en: 'Unbreakable' },
+  TitleData_SigmaStriker: { en: 'Sigma Striker' },
+  TitleData_SpeedySkirmisherMastery: { en: 'Prodigy' },
+  TitleData_StalwartProtectorMastery: { en: 'The Most Huggable' },
+  TitleData_Striker: { en: 'The Eager Rookie' },
+  TitleData_TeamEDMOni: { en: 'Thousand Absolutes' },
+  TitleData_TeamRockOni: { en: 'Vice Versa' },
+  TitleData_TeaTime_Boba: { en: 'Boba' },
+  TitleData_TeaTime_Caffeinated: { en: 'Caffeinated' },
+  TitleData_TeaTime_Jelly: { en: 'Jelly' },
+  TitleData_TempoSniperMastery: { en: 'Fashion Icon' },
+  TitleData_TournamentShadowHelper: { en: 'Shadow Helper' },
+  TitleData_TournamentShadowHero: { en: 'Shadow Hero' },
+  TitleData_TournamentShadowLoser: { en: 'World\'s Best Loser' },
+  TitleData_TournamentShadowSelfless: { en: 'The Selfless' },
+  TitleData_TournamentShadowWarrior: { en: 'Shadow Warrior' },
+  TitleData_Trailblazer: { en: 'Trailblazer' },
+  TitleData_WhipFighterMastery: { en: 'Off the Hook' },
 }
 
 export function getTitleLocale(id: string, locale = 'en') {
