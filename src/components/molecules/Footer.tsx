0dc5c1a1a42415635acafa026e4f123ca51c7cd4
Nodgear <wbm.nodge@gmail.com>
2023-07-05 00:45:40 -0300

    fix: footer links

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
         Strikr © 2023
       </p>
       <ul className='flex gap-8 text-white font-semibold mt-4'>
-        <li><Link href='https://strikr.gg/grpahql'>API</Link></li>
+        <li><Link href='https://api.strikr.gg'>API</Link></li>
         <li><Link href='http://discord.com/users/175314117924487168'>Contact the developer</Link></li>
       </ul>
     </footer>
